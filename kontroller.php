33c38d2d4b57d7e9a5239630d1253706376fab0d
kirkeN <kirke.narusk@gmail.com>
2016-04-03 22:45:26 +0300

    php vol1

---
+++ kontroller.php
@@ -0,0 +1,15 @@
+<?php
+$pildid=array(
+    array("big"=>"korvarongad1.jpg", "small"=>"pisipilt/korvarongad1.jpg", "alt"=>"kõrvarõngad"),
+    array("big"=>"lilled1.jpg", "small"=>"pisipilt/lilled1.jpg", "alt"=>"lilled"),
+    array("big"=>"kindad1.jpg", "small"=>"pisipilt/kindad1.jpg", "alt"=>"kindad"),
+    array("big"=>"sokid1.jpg", "small"=>"pisipilt/sokid1.jpg", "alt"=>"sokid"),
+    array("big"=>"pudelisuuvoo.jpg", "small"=>"pisipilt/pudelisuuvoo.jpg", "alt"=>"pudelisuuvöö"),
+    array("big"=>"kangasteljed1.jpg", "small"=>"pisipilt/kangasteljed1.jpg", "alt"=>"kangastelje tööd")
+);
+
+
+include("tooted.html");
+
+
+?>
